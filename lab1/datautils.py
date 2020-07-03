bea712ea37d59270bba24b5efe00219c924b622b
mshagirov <muratshagirov@gmail.com>
2020-07-03 17:24:37 +0800

    added diffs feature

---
+++ lab1/datautils.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+def windowed_data(x_norm, lead_time=5, window_size=6):
+    '''Prepare windowed data with a given window size
+    - columns:`[Y(T+lead_time), Y(T+0), ...,Y(T-window_size+1)]`
+    '''
+    start_time = window_size-1
+    X = [x_norm[start_time+lead_time:].reshape(-1,1) ] # Y(T+lead_time)
+    # append Y(T+0) to Y(T-window_size+1)
+    for w in range(window_size,0,-1):
+        X.append(x_norm[w-1:x_norm.shape[0] - lead_time - window_size+w].reshape(-1,1))
+    # concatenate Y(T+lead_time), Y(T+0),...,Y(T-window_size+1)
+    return np.concatenate(X,axis=1)
+
+
+def windowed_diff_data(x_norm, lead_time=5, window_size=6):
+    '''Prepare windowed data with a given window size
+    - 1st and 2nd columns:`[Y(T+lead_time), Y(T+0),]
+    - 3rd and subsequent columns are differences:
+    `[, Y(T+0)-Y(T-1), Y(T-1)-Y(T-2),..., Y(T-window_size+2)-Y(T-window_size+1)]`
+    '''
+    X = windowed_data(x_norm, lead_time=lead_time, window_size=window_size)
+    X[:,2:] = X[:,1:-1]-X[:,2:]
+    # concatenate Y(T+lead_time), Y(T+0), differences
+    return X
